bf6e35d73f919ce71de82683aa47a42655858c8a
Maksim Sheko <uchihaboy95@gmail.com>
2021-11-26 22:32:54 +0300

    fix

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-let title = "glo";
-let screens = "Простые, Сложные, Интерактивные";
-let lowerScreens = screens.toLowerCase();
-let lowerScreensData = lowerScreens.split(", ");
-let screenPrice = 50;
-let rollback = 35;
-let fullPrice = 1000;
-let adaptive = true;
+const title = "glo";
+const screens = "Простые, Сложные, Интерактивные";
+const lowerScreens = screens.toLowerCase();
+const screenPrice = 50;
+const rollback = 35;
+const fullPrice = 1000;
+const adaptive = true;
 
 
 console.log(typeof title);
-console.log(screens.length);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
-
+console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
 console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
+console.log(lowerScreens.split(", "));
+console.log(fullPrice * (rollback/100));
 
-console.log(lowerScreensData)
 
-let back = () => {
-    return fullPrice * (rollback/100)
-}
-
-console.log(back());
